1eceb2de7009bb03306db29d6c16692ab6dce627
selmik26 <tlyavsinr@mail.ru>
2024-05-15 20:40:50 +0500

    LOl

--- parser.py
+++ parser.py
@@ -32,14 +32,15 @@ def connect(userid):
             print(e)
             print()
             i += 1
-            time.sleep(20)
+
+
+#            time.sleep(20)
 
 
 def extraction(soup):
     pub_list = list()
     table = soup.find_all('table')[1]
     publications = table.find_all("tr")
-    ##    i = 1
     for pub in publications:
         inf = pub.find_all('td')[1]
         inf_text = list(inf.strings)
@@ -59,12 +60,6 @@ def extraction(soup):
             name = inf_text[ind].strip()
             author = inf_text[ind + 1].strip()
             place = inf_text[ind + 2].strip().replace("\xa0", " ").replace("\r\n", "")
-        ##        print(i)
-        ##        print(name)
-        ##        print(author)
-        ##        print(place)
-        ##        print()
-        ##        i+=1
         pub_list.append({
             "name": name,
             "author": author,
@@ -74,12 +69,26 @@ def extraction(soup):
 
 
 FileID = open("ID.txt", 'r')
+FilePub = open("publications.txt", 'w')
 for userid in FileID:
+    userid = userid.strip()
     print(userid)
     soup = connect(userid)
+    while soup.title.text == 'Тест Тьюринга':
+        print("Зайдите на сайт и пройдите Тест Тьюринга")
+        input("Нажмите enter чтобы продолжить\n")
+        soup = connect(userid)
+        time.sleep(20)
     publication_list = extraction(soup)
-    print(publication_list)
+    FilePub.write('_' * 50 + userid + '_' * 50 + '\n')
+    FilePub.write(str(len(publication_list)) + " публикаций\n")
+    for i in range(len(publication_list)):
+        FilePub.write(str(i + 1) + " " + publication_list[i]['name'] + "\n")
+        FilePub.write(publication_list[i]['author'] + "\n")
+        FilePub.write(publication_list[i]['place'] + "\n")
+
 FileID.close()
+FilePub.close()
 
 # id155572 - Васильев Денис Юрьевич
 # id112663 - Картак Вадим Михайлович
